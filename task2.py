449d9747e24c1eeeecba79f1e0a1e2775a68eca8
slin35 <slin35@calpoly.edu>
2020-05-29 00:34:15 -0700

    updated task2

--- task2.py
+++ task2.py
@@ -8,56 +8,89 @@ _post = ';session-id=31337'
 
 _IV = os.urandom(16)
 _key = os.urandom(16)
+_str_IV = "".join(chr(c) for c in _IV)
+_str_key = "".join(chr(c) for c in _key)
+
+cipher = AES.new(_key, AES.MODE_ECB)
 
 
 def submit(user_string: str) -> bytearray:
     query = _pre + user_string + _post
     url_encoded = urllib.parse.quote(query)
-    return aes_128_cbc(url_encoded, _IV, _key)
+    print(url_encoded)
+    return aes_128_cbc(url_encoded)
 
 
-def aes_128_cbc(plain_text: str, IV:str, key:str) -> bytearray:
+def aes_128_cbc(plain_text: str) -> bytearray:
+    plain_text = bytes(plain_text, 'utf-8')
     blocks = [plain_text[i*16:(i+1)*16] for i in range(int(len(plain_text)/16))]
     last_block = plain_text[len(blocks) * 16:]
+    result = bytearray()
 
     if len(last_block) > 0:
-        blocks.append(PKCS_7_padding(last_block, 16))
+        blocks.append(PKCS_7_padding_encode(last_block, 16))
 
-    # encrypt the first block
-    cipher = AES.new(key=key, mode=AES.MODE_ECB)
-    cipher_text = xor(blocks[0], bytearrayToString(_IV))
-    cipher_array = [ord(ch) for ch in cipher_text]
-    cipher_text = cipher.encrypt(bytes(cipher_array))
-    result = cipher_text
+    for i in range(len(blocks)):
+        if i == 0:
+            encrypted_text = xor(blocks[i].decode('utf-8'), _str_IV)
+        else:
+            encrypted_text = xor(blocks[i].decode('utf-8'),  bytearrayToString(encrypted_text))
 
-    for i in range(1, len(blocks)):
-        cipher_text = xor(blocks[i], bytearrayToString(cipher_text))
-        cipher_array = [ord(ch) for ch in cipher_text]
-        cipher_text = cipher.encrypt(bytes(cipher_array))
-        result += cipher_text
+        encrypted_text = [ord(c) for c in encrypted_text]
+        encrypted_text = cipher.encrypt(bytes(encrypted_text))
+        result += encrypted_text
 
     return result
 
 
-def xor(str1:str, str2:str) -> str:
+def verify(encoded_string:bytearray) -> bool:
+    result = ""
+    blocks = [encoded_string[i*16:(i+1)*16] for i in range(int(len(encoded_string)/16))]
+    
+    for i in range(len(blocks)):
+        decrypted_text = cipher.decrypt(blocks[i])
+        decrypted_text = bytearrayToString(decrypted_text)
+        if i == 0:
+            decrypted_text = xor(decrypted_text, _str_IV)
+        else:
+            decrypted_text = xor(decrypted_text, bytearrayToString(blocks[i-1]))
+
+        if i == len(blocks) - 1:
+            decrypted_text = PKCS_7_padding_decode(decrypted_text)
+        
+        result += decrypted_text
+
+    return ';admin=true;' in result
+
+
+def xor(str1:str, str2:str):
     if len(str1) != len(str2):
         print("unequal string lengths")
         exit(1)
     return "".join(chr(ord(x) ^ ord(y)) for (x, y) in zip(str1, str2))
 
 
-def PKCS_7_padding(block:str, block_size:int) -> str:
+def PKCS_7_padding_encode(block:bytearray, block_size:int) -> bytearray:
     to_pad = block_size - len(block) % block_size
-    pad = chr(to_pad)
+    pad = bytes(chr(to_pad), 'utf-8')
     return block + pad * to_pad
 
+'''
+def PKCS_7_padding_decode(bytes:bytearray) -> bytearray:
+    pad = ord(bytes.decode('utf-8')[-1])
+    return bytes[:-pad]
+'''
+def PKCS_7_padding_decode(bytes:str) -> str:
+    pad = ord(bytes[-1])
+    return bytes[:-pad]
 
-def bytearrayToString(barray:bytearray) -> str:
+def bytearrayToString(barray:bytes) -> str:
     return "".join(chr(b) for b in barray)
 
 
 def main():
-    print(submit("hello"))
+   a = submit("hello;admin=true;")
+   print(verify(a))
 
 
 if __name__ == '__main__':
